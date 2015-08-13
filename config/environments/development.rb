99bed6216d11f020dcfef10fc4319da7f98a9c5f
Dan Mashuda <dmashuda@ycp.edu>
2015-08-12 23:28:51 -0400

    adjust config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,7 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  config.action_mailer.default_url_options = { host: 'danphotoshare.herokuapp.com'}
-  Rails.application.routes.default_url_options[:host] = 'danphotoshare.herokuapp.com'
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
